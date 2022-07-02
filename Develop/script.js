3006b794602449617848a0a217ee190009d1d852
Priddle88 <96860899+Priddle88@users.noreply.github.com>
2022-07-02 09:53:40 -0600

    Length of password done

--- Develop/script.js
+++ Develop/script.js
@@ -11,18 +11,23 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var length = prompt("How long would you like your password? (8 - 128 characters");
-
-  if (length < 8 || length > 128) {
-    length = prompt("Try Again. How long would you like your password? (8 - 128 characters");
+  function findLength() {
+    var length = prompt("How long would you like your password? (8 - 128 characters");
+    if (length < 8 || length > 128) {
+      length = confirm("Try Again. You need to choose a number between 8 - 128");
+      findLength();
+    }
   }
+
+  findLength();
+
   
   var lower = confirm("Would you like LOWERCASE letters in your password?");
   var upper = confirm("Would you like UPPERCASE letters in your password?");
   var numbers = confirm("Would you like NUMBERS in your password?");
   var special = confirm("Would you like SPECIAL characters in your password?");
 
-  
+
 
   return;
 }
